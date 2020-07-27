2f16ab5275c182182bc3ecbe215c6137377e2e77
Abdel ADAM <adamdjamal18@gmail.com>
2020-07-27 08:43:54 +0100

    init: taskflow-app with React + TypeScript

---
+++ README.md
@@ -0,0 +1,16 @@
+# TaskFlow
+
+Kanban board app built with React and TypeScript.
+
+## Stack
+
+- React 16.13
+- TypeScript (strict)
+- Create React App
+
+## Getting Started
+
+```bash
+npm install
+npm start
+```
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>TaskFlow</h1>
+        <p>Kanban board — React + TypeScript</p>
+      </header>
+      <main className="app-main">
+        <p>Board coming soon...</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
